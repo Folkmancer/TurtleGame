269956e0db2dba0d944c294fbca92ba264d52795
Artyom Dolgushin <folkmancer@gmail.com>
2018-05-30 13:11:02 +0500

    Creating a turtle, choice of direction and creating food

--- TurtleGame/Program.cs
+++ TurtleGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
 
 namespace TurtleGame
 {
@@ -10,6 +11,22 @@ namespace TurtleGame
     {
         static void Main(string[] args)
         {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+            var eat = Shapes.AddRectangle(10, 10);
+            Shapes.Move(eat, 200, 200);
+            while (true)
+            {
+                Turtle.Move(10);
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up") Turtle.Angle = 0;
+            else if (GraphicsWindow.LastKey == "Right") Turtle.Angle = 90;
+            else if (GraphicsWindow.LastKey == "Down") Turtle.Angle = 180;
+            else if (GraphicsWindow.LastKey == "Left") Turtle.Angle = 270;
         }
     }
 }
